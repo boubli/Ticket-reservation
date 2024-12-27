b7b814705404c73f74781e48052942ff9702b1aa
Youssef Boubli <bbb.vloger@gmail.com>
2024-12-27 12:16:18 +0000

    Update

--- dbConnect.py
+++ dbConnect.py
@@ -1,16 +1,20 @@
 import sqlite3
+
 class DBBConnect:
     def __init__(self):
-        self._db=sqlite3.connect("resela.db")
+        self._db = sqlite3.connect("resela.db")
         self._db.row_factory = sqlite3.Row
-        self._db.execute("create table if not exists Ticket(ID integer primary key autoincrement, Name text, Gender text, Comment text)")
+        self._db.execute(
+            "CREATE TABLE IF NOT EXISTS Ticket (ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Gender TEXT, Comment TEXT)"
+        )
         self._db.commit()
 
     def Add(self, Name, Gender, Comment):
-        self._db.execute("insert into Ticket(Name,Gender,Comment) values(?,?,?)",(Name,Gender,Comment))
+        self._db.execute(
+            "INSERT INTO Ticket (Name, Gender, Comment) VALUES (?, ?, ?)", (Name, Gender, Comment)
+        )
         self._db.commit()
-        return "request is submitted"
+        return "Request has been submitted successfully."
 
     def ListRequest(self):
-        cursor=self._db.execute("Select * from Ticket")
-        return cursor;
+        return self._db.execute("SELECT * FROM Ticket")
