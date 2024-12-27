80b313a7d73b560b1eca26d480c6e8cf382da4b0
Youssef Boubli <bbb.vloger@gmail.com>
2024-12-27 12:15:56 +0000

    Update

--- ListRequist.py
+++ ListRequist.py
@@ -1,30 +1,27 @@
 from tkinter import *
 from tkinter import ttk
 from dbConnect import DBBConnect
-dBConnect=DBBConnect()
+
+dBConnect = DBBConnect()
 
 class LisTicket:
 
     def __init__(self):
-        self._root=Tk()
-        self._root.title("List")
-        self._dBConnect=DBBConnect()
-        tv=ttk.Treeview(self._root)
-        tv.pack()
-        tv.heading('#0', text='ID')
-        tv.configure(column=('#Name','#Gender','#Comment'))
-        tv.heading('#Name', text='Name')
-        tv.heading('#Gender', text='Gender')
-        tv.heading('#Comment', text='Comment')
-        cursor=self._dBConnect.ListRequest()
+        self._root = Toplevel()
+        self._root.title("Reservation List")
+        self._root.geometry("600x400")
+
+        # Treeview
+        tv = ttk.Treeview(self._root, columns=("#Name", "#Gender", "#Comment"), show='headings')
+        tv.heading("#Name", text="Name")
+        tv.heading("#Gender", text="Gender")
+        tv.heading("#Comment", text="Comment")
+        tv.column("#Name", width=150)
+        tv.column("#Gender", width=100)
+        tv.column("#Comment", width=300)
+        tv.pack(fill=BOTH, expand=True)
+
+        # Fetch data
+        cursor = dBConnect.ListRequest()
         for row in cursor:
-            tv.insert('','end','#{}'.format(row['ID']),text=row['ID'])
-            tv.set('#{}'.format(row['ID']),'#Name', row['Name'])
-            tv.set('#{}'.format(row['ID']),'#Gender', row['Gender'])
-            tv.set('#{}'.format(row['ID']),'#Comment', row['Comment'])
-
-        self._root.mainloop()
-
-
-
-
+            tv.insert('', 'end', values=(row['Name'], row['Gender'], row['Comment']))
